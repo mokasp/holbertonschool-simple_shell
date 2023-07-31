e2a689fbc4c018524d804e23479a9b322630a080
mokasp <125315163+mokasp@users.noreply.github.com>
2023-07-31 14:44:04 -0500

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+#define BUFFER 1024
+
+#define MAXTOK 64
+
+#define EXIT "exit\n"
+
+#endif
