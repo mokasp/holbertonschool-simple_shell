7745242e32852f6b571ac48c9b8f5d1c5e977b42
mokasp <kasperbouldin@gmail.com>
2023-08-01 13:05:13 -0700

    parsing and main loop

--- shell.h
+++ shell.h
@@ -6,11 +6,15 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <sys/wait.h>
 
 #define BUFFER 1024
 
 #define MAXTOK 64
 
 #define EXIT "exit\n"
+
+char **parse(char *str);
+extern char **environ;
 
 #endif
