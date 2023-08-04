8c49c93c09b40873d78fa11f6e1871b6f4fd5f39
root <Frankblation>
2023-08-04 11:18:44 -0700

    Things are a little cleaner Shell_runner is where i've been compiling.

--- parse.c
+++ parse.c
@@ -2,36 +2,22 @@
 
 char **parse(char *str)
 {
-<<<<<<< HEAD
+
 	int i = 0;
 	int bufsize = TOK_BUFSIZE;
 	char **toks = malloc(bufsize * sizeof(char *));
 	char *token;
-=======
-	char *token = NULL;
-	char **toks = NULL;
-	int i;
->>>>>>> origin
 
 	token = strtok(str, " ");
 	while (token != NULL)
 	{
-<<<<<<< HEAD
-		toks[i] = token;
-=======
-		toks[i] = strdup(token);
 
->>>>>>> origin
+		toks[i] = token;
+
 		token = strtok(NULL, " ");
 		i++;
 	}
 	toks[i] = token;
-<<<<<<< HEAD
+
 	return (toks);
 }
-=======
-	free(token);
-	free(toks);
-	return (toks);
-}
->>>>>>> origin
