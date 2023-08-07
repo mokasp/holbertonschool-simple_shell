df7239a7420f1e1e6927a737f4352af35ed84261
mokasp <125315163+mokasp@users.noreply.github.com>
2023-08-06 22:47:19 -0500

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -1,33 +1,70 @@
 #include "shell.h"
 
-int builtIn(char **parsed)
+int cmdHandler(char **parsed)
 {
-        int i;
-        char *numOfCmds = 4;
         int switchArg = 0;
-        char *ownCmdList[numOfCmds];
+        char **env = environ;
+        char **envp = environ;
+        int i, j;
+        char *cmdList[6];
+        int status = 0;
 
-        ownCmdList[0] = "exit";
-        ownCmdList[1] = "";
-        ownCmdList[2] = "";
-        ownCmdList[3] = "";
+        cmdList[0] = "exit";
+        cmdList[1] = "hi";
+        cmdList[2] = "help";
+        cmdList[3] = "cd";
+        cmdList[4] = "env";
+        cmdList[5] = "path";
 
-
-        for (i = 0; i < numOfCmds; i++)
+        for (i = 0; i < 6; i++)
         {
-                if (strcmp(parsed[0], ownCmdList[i]) == 0)
+                if (strcmp(parsed[0], cmdList[i]) == 0)
                 {
                         switchArg = i + 1;
-                        break;
                 }
         }
 
         switch (switchArg)
         {
-                if (strcmp(line, EXIT) == 0)
-                {
-                        printf("baiiii\n");
-                        exit (EXIT_SUCCESS);
-                }
+                case 1:
+                        printf("ahhhh bai \n");
+                        return (-1);
+                case 2:
+                        printf("\n oh hallo \n");
+                        return (1);
+                case 3:
+                        printf("wip\n");
+                        return (1);
+                case 4:
+                        if (parsed[1] == NULL)
+                                perror("too few arguments");
+                        else
+                        {
+                                if (chdir(parsed[1]) != 0)
+                                {
+                                        perror("hsh");
+                                }
+                        }
+                        return (1);
+                case 5:
+                        while (*env != NULL)
+                        {
+                                printf("%s\n", *env);
+                                env++;
+                        }
+                        return (1);
+                case 6:
+                        for (j = 0; envp[j] != NULL; j++)
+                        {
+                                if (strncmp(envp[j], "PATH=", 5) == 0)
+                                {
+                                        printf("%s\n", &envp[j][5]);
+                                        break;
+                                }
+                        }
+                default:
+                        status = execution(parsed);
+                        return (status);
         }
+        return (0);
 }
