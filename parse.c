1fe9c0bab3f06bcd3bf9e1009ebf237a990c95d6
root <Frankblation>
2023-08-07 20:38:01 -0700

    this is a commit to update readme and fix problems

--- parse.c
+++ parse.c
@@ -13,7 +13,6 @@ char **parse(char *str)
                 exit(EXIT_FAILURE);
         }
 
-<<<<<<< HEAD
         token = strtok(str, " ");
         while (token != NULL)
         {
@@ -25,26 +24,3 @@ char **parse(char *str)
         free(token);
         return (toks);
 }
-=======
-	token = strtok(str, TOK_DELIM);
-	while (token != NULL)
-	{
-		toks[i] = token;
-		i++;
-		if (i >= bufsize)
-		{
-			bufsize += TOK_BUFSIZE;
-			toks = realloc(toks, bufsize * sizeof(char *));
-			if (!toks)
-			{
-				perror("allocation error\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-		token = strtok(NULL, TOK_DELIM);
-	}
-	toks[i] = NULL;
-	free(token);
-	return (toks);
-}
->>>>>>> e995c0c30cb034b9d38af900ce42195032655d5a
--- shell.c
+++ shell.c
@@ -2,34 +2,22 @@
 
 int main(void)
 {
-        char **argv;
-        char *line;
+	char **argv;
+	char *line;
+	int status = 1;
 
 
-        while (1)
-        {
-                printf(":3 ");
+	while (status = 1)
+	{
+		if (isatty(fileno(stdin)))
+		printf(":3 ");
 
-<<<<<<< HEAD
-                line = readLine();
-                argv = parse(line);
-=======
 		line = readLine();
 		argv = parse(line);
 		status = cmdHandler(argv);
-<<<<<<< HEAD
-		if (status == -2)
-		{
-			free(line);
-			free(argv);
-			exit(2);
-		}
->>>>>>> fd4849020e48beebab598bace3050ceaf35a3c7c
-=======
->>>>>>> e995c0c30cb034b9d38af900ce42195032655d5a
 
-                free(line);
-                free(argv);
-        }
-        exit(EXIT_SUCCESS);
+		free(line);
+		free(argv);
+	}
+	exit(EXIT_SUCCESS);
 }
